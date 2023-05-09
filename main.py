a933ed36da5247130e73f72e6c1321caecf92d42
FifiMelo <felipe336336@gmail.com>
2023-05-09 16:27:30 +0200

    downloaded actors ids

---
+++ main.py
@@ -0,0 +1,19 @@
+
+import json
+import requests
+
+url = "https://online-movie-database.p.rapidapi.com/actors/list-most-popular-celebs"
+
+querystring = {"homeCountry":"US","currentCountry":"US","purchaseCountry":"US"}
+
+headers = {
+	"X-RapidAPI-Key": "d38fac4a46mshcae3c95dd27dd25p19e3e2jsnb41485bca785",
+	"X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+actor_ids = [actor_id[6:15] for actor_id in response.json()]
+
+with open("actor_ids.json", "w") as json_file:
+    json.dump(actor_ids, json_file)
